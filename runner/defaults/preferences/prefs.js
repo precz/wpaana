7a10b10373ff366dca19c4f249119befccb57a95
Tim Retout <tim@retout.co.uk>
2019-01-29 17:35:49 +0000

    Make User-Agent string compatible with Firefox
    Fixes #3

--- runner/defaults/preferences/prefs.js
+++ runner/defaults/preferences/prefs.js
@@ -5,3 +5,4 @@ pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
 pref("nglayout.debug.disable_xul_cache", true);
 pref("nglayout.debug.disable_xul_fastload", true);
+pref("general.useragent.compatMode.firefox", true);
